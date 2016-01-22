0b665aa8ddb2975c26e48b080659f9a8b6031484
Julian Kleine <info@lxdriver.de>
2016-01-22 03:09:23 +0100

    Plan

---
+++ coffee.plan.coffee
@@ -0,0 +1,13 @@
+Pot = Pot or {}
+
+class Pot.Plan
+    constructor: (@data, @maxDays = 31) ->
+        @days = @data.days
+        @month = @data.month
+        
+        @worker = @data.worker
+        
+        for worker in @worker
+            worker = new Pot.Worker worker.id, worker.name, worker.role
+        
+    getDay: -> @day
---
+++ coffee.plan.js
@@ -0,0 +1,26 @@
+var Pot;
+
+Pot = Pot || {};
+
+Pot.Plan = (function() {
+  function Plan(data, maxDays) {
+    var worker, _i, _len, _ref;
+    this.data = data;
+    this.maxDays = maxDays != null ? maxDays : 31;
+    this.days = this.data.days;
+    this.month = this.data.month;
+    this.worker = this.data.worker;
+    _ref = this.worker;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      worker = _ref[_i];
+      worker = new Pot.Worker(worker.id, worker.name, worker.role);
+    }
+  }
+
+  Plan.prototype.getDay = function() {
+    return this.day;
+  };
+
+  return Plan;
+
+})();
